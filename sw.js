f04b45762c1b7cd68e6d0ca567947fd46ca56c42
Surajprem7 <Surajprem7@users.noreply.github.com>
2026-06-23 11:41:16 +0530

    Hide registration form for players who already registered on this device

--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ self.addEventListener('notificationclick', e => {
   e.waitUntil(self.clients.openWindow('./'));
 });
 
-const CACHE = 'eleague-v23';
+const CACHE = 'eleague-v24';
 const ASSETS = [
   './',
   './index.html',
