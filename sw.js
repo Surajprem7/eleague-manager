13ac3ffa6a7116148618e1fb7e76dab2fa1c9243
Surajprem7 <Surajprem7@users.noreply.github.com>
2026-06-25 19:29:44 +0530

    Bump to v38 to test update banner fix

--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ self.addEventListener('notificationclick', e => {
   e.waitUntil(self.clients.openWindow('./'));
 });
 
-const CACHE = 'eleague-v37';
+const CACHE = 'eleague-v38';
 const ASSETS = [
   './',
   './index.html',
